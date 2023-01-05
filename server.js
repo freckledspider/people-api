5a169e364548cf46759b8b04f16c258aef964114
bri <bcamesi@gmail.com>
2023-01-04 20:52:43 -0500

    find and create

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors")
+const morgan = require("morgan")
 
 const { PORT = 3000, DATABASE_URL } = process.env;
 
@@ -18,8 +19,38 @@ mongoose.connection
     .on("error", (error) => console.log(error))
 
 
+const PeopleSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    title: String,
+})
+
+const People = mongoose.model("People", PeopleSchema)
+
+// middleware
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
+// routes
 app.get("/", (req, res) => {
     res.send("hello world")
+})
+
+app.get("/people", async (req, res) => {
+    try{
+        res.json(await People.find({}))
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})
+
+app.post("/people", async (req, res) => {
+    try{  
+        res.json(await People.create(req.body))
+    } catch(error) {
+        res.status(400).json(error)
+    }
 })
 
 

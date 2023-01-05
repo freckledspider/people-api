427316e4fc7bfbc70edd7305e588e5a963470e67
bri <bcamesi@gmail.com>
2023-01-04 20:14:37 -0500

    connect to mongodb

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
-require("dotenv").config();
-const {PORT = 3000} = process.env
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+const { PORT = 3000, DATABASE_URL } = process.env;
+
+const app = express();
+
+mongoose.connect(DATABASE_URL, {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true
+});
+
+mongoose.connection
+    .on("open", () => console.log('You are connnectd to mongoose'))
+    .on("close", () => console.log('You are disconnected from mongoose'))
+    .on("error", (error) => console.log(error))
+
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
